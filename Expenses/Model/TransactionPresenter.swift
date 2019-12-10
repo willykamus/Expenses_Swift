1288f73d634e778f54a71bf9e819adab203a6e9e
William Ching <wching.g@gmail.com>
2019-12-09 22:21:04 -0500

    Apply ViewModel to Transaction

---
+++ Expenses/Model/TransactionPresenter.swift
@@ -0,0 +1,36 @@
+//
+//  TransactionModel.swift
+//  Expenses
+//
+//  Created by William Ching on 2019-12-09.
+//  Copyright © 2019 William Ching. All rights reserved.
+//
+
+import Foundation
+
+class TransactionPresenter {
+    
+    private var transaction: Transaction
+    
+    init(withTransaction transaction:Transaction) {
+        self.transaction = transaction
+    }
+    
+    func transactionAmount() -> String? {
+        
+        guard let value = transaction.amount else {
+            return nil
+        }
+        
+        return String(value)
+    }
+    
+    func transactionCategory() -> String? {
+        return transaction.category?.rawValue
+    }
+    
+    func transactionType() -> String? {
+        return transaction.transactionType?.rawValue
+    }
+    
+}
